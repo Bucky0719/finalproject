e82de3d9b7505384c9d0c9762f4776afb721723f
Bucky0719 <rahulnagarajan19@gmail.com>
2024-01-08 11:31:25 +0000

    new update

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-if [[ $GIT_BRANCH == "orgin/dev" ]];
-then
+if [[ $GIT_BRANCH == "orgin/dev" ]]; then
+
      ./build.sh     
      docker login -u bucky0838 -p dckr_pat_79C2h7PDN21tTnkWp-4-xSNlHIg
-     docker tag reactjs-demo_reactjs-image_1 bucky0838/dev
+     docker tag reactjs-demo_reactjs-image bucky0838/dev
      docker push bucky0838/dev 
 
 else [[ $GIT_BRANCH == "orgin/main" ]];
-then
+
       ./build.sh 
       docker login -u bucky0838 -p dckr_pat_79C2h7PDN21tTnkWp-4-xSNlHIg
-      docker tag reactjs-demo_reactjs-image_1 bucky0838/prod
+      docker tag reactjs-demo_reactjs-image bucky0838/prod
       docker push bucky0838/prod
 fi
