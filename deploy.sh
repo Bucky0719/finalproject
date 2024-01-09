42b828d6928fe570886fedbce878329f8f167f3d
Bucky0719 <100702108+Bucky0719@users.noreply.github.com>
2024-01-09 12:20:51 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-if [[ $GIT_BRANCH == "origin/dev" ]]; then
+docker login -u bucky0838 -p dckr_pat_79C2h7PDN21tTnkWp-4-xSNlHIg
 
-     ./build.sh     
-     docker login -u bucky0838 -p dckr_pat_79C2h7PDN21tTnkWp-4-xSNlHIg
-     docker tag reactjs-demo_reactjs-image bucky0838/dev
-     docker push bucky0838/dev 
+if [[ $GIT_BRANCH == "origin/dev" ]]; then
+    chmod +x build.sh
+    ./build.sh
+    docker tag reactjs-demo_reactjs-image bucky0838/dev
+    docker push bucky0838/dev
 
 elif [[ $GIT_BRANCH == "origin/main" ]]; then
+    chmod +x build.sh
+    ./build.sh
+    docker tag reactjs-demo_reactjs-image bucky0838/prod
+    docker push bucky0838/prod
 
-      ./build.sh 
-      docker login -u bucky0838 -p dckr_pat_79C2h7PDN21tTnkWp-4-xSNlHIg
-      docker tag reactjs-demo_reactjs-image bucky0838/prod
-      docker push bucky0838/prod
-else 
-      echo "failed"
+else
+    echo "Deploy Error. Image not pushed."
 fi
