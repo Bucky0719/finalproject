c1ac2c73f3a7646a36d330d4d35415f49b11ac12
Bucky0719 <100702108+Bucky0719@users.noreply.github.com>
2024-01-09 11:28:00 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-if [[ $GIT_BRANCH == "orgin/dev" ]]; then
+if [[ $GIT_BRANCH == "origin/dev" ]]; then
 
      ./build.sh     
      docker login -u bucky0838 -p dckr_pat_79C2h7PDN21tTnkWp-4-xSNlHIg
      docker tag reactjs-demo_reactjs-image bucky0838/dev
      docker push bucky0838/dev 
 
-else [[ $GIT_BRANCH == "orgin/main" ]];
+elif [[ $GIT_BRANCH == "origin/main" ]];
 
       ./build.sh 
       docker login -u bucky0838 -p dckr_pat_79C2h7PDN21tTnkWp-4-xSNlHIg
       docker tag reactjs-demo_reactjs-image bucky0838/prod
       docker push bucky0838/prod
+else 
+echo "failed"
 fi
